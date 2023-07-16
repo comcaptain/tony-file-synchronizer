b157d4f7e0c19744978fb419e186bc85e02fcb65
Tony <htmlcaptain@gmail.com>
2023-07-16 18:03:27 +0900

    Use SLF4J to do logging

---
+++ src/main/java/com/sgq/tool/log/TLogger.java
@@ -0,0 +1,9 @@
+package com.sgq.tool.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface TLogger
+{
+	Logger TLog = LoggerFactory.getLogger(TLogger.class);
+}
--- src/main/java/com/sgq/tool/sync/App.java
+++ src/main/java/com/sgq/tool/sync/App.java
@@ -3,11 +3,14 @@ package com.sgq.tool.sync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.sgq.tool.log.TLogger.TLog;
+
 @SpringBootApplication
 public class App
 {
 	public static void main(final String[] args)
 	{
+		TLog.info("Test {}", "abc");
 		SpringApplication.run(App.class, args);
 	}
 }
